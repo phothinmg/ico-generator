e3ccc179cdcf45321bfe7e2484242cfc7fe9d182
phothinmg <phothinmg@gmail.com>
2024-06-28 11:55:23 +0630

    modified:   client/home.tsx

--- client/home.tsx
+++ client/home.tsx
@@ -9,11 +9,6 @@ const Home: FC = () => {
   return (
     <Layout title="Ico Generator">
       <h2>Image to favicon generator</h2>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFpbYzWHR8Yr4rACZjkWa-mITbmjY1tuy1_Q&s"
-        alt="ico"
-        class="hero"
-      />
       <hr />
       <ImageUpload />
     </Layout>
